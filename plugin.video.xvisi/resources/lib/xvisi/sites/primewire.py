a085f7642c2613b378c0e359fb90b87703fbb8b0
Marc Brinkmann <git@marcbrinkmann.de>
2013-10-03 01:10:13 +0200

    PrimeWire gets sources correctly.

--- plugin.video.xvisi/resources/lib/xvisi/sites/primewire.py
+++ plugin.video.xvisi/resources/lib/xvisi/sites/primewire.py
@@ -1,8 +1,44 @@
+from urlparse import urlparse, urljoin
+
+from lxml.html import fromstring
+
 from .vodly import Vodly
+from ..web import web
 
 
 class PrimeWire(Vodly):
-    _BASEURL = 'http://primewire.ag'
+    _BASEURL = 'http://www.primewire.ag'
     name = 'PrimeWire.ag'
     short_name = 'primewire'
     id = 'primewire'
+
+    def get_sources(self, key):
+        resp = web.get(key)
+
+        root = fromstring(resp.text)
+
+        for link in root.cssselect(
+            'table.movie_version .movie_version_link a'
+        ):
+            link = link.attrib['href']
+
+            if not link.startswith('/external.php'):
+                continue
+
+            # FIXME: could check for redirects here
+            resp = web.get(urljoin(self._BASEURL, link))
+
+            if not resp.url.startswith(self._BASEURL):
+                # we got the url directly, rejoice
+                yield resp.url, urlparse(resp.url).netloc
+
+            exroot = fromstring(resp.text)
+
+            # get last frame's href
+            frames = exroot.cssselect('frame')
+            if not frames:
+                # not a source site (no frames)
+                continue
+
+            href = frames[-1].attrib['src']
+            yield href, urlparse(href).netloc
