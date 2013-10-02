9b0096d6f9788f52a53cbe6a3c8e3138c906ffa6
Marc Brinkmann <git@marcbrinkmann.de>
2013-10-02 02:09:12 +0200

    Complete support for putlocker.com.

--- plugin.video.xvisi/resources/lib/xvisi/sources/putlocker.py
+++ plugin.video.xvisi/resources/lib/xvisi/sources/putlocker.py
@@ -1,5 +1,7 @@
 import re
-from urlparse import urlparse
+from urlparse import urlparse, urljoin
+
+from lxml.html import fromstring
 
 from ..base import VideoSource
 from ..web import web
@@ -23,4 +25,26 @@ class PutlockerComSource(VideoSource):
         return 'putlocker.com' in u.netloc
 
     def get_video_url(self, url):
-        raise NotImplementedError('ooops')
+        resp = web.post(url, data={'freeuser': 'yes'})
+
+        # landiong page, get hash
+        root = fromstring(resp.text)
+        hash = root.cssselect('input[name=hash]')[0].attrib['value']
+
+        resp = web.post(
+            url,
+            data={'hash': hash, 'confirm': 'Continue as Free User'}
+        )
+
+        root = fromstring(resp.text)
+
+        # not supported by lxml version in system-wide use?
+        #relurl = root.cssselect(
+        #    'a:contains("Download File")'
+        #)[0].attrib['href']
+
+        for a in root.cssselect('a'):
+            href = a.attrib.get('href')
+
+            if href and href.startswith('/get_file.php'):
+                return urljoin(url, href)
