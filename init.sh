abe64b16beb8d30fdb135748a7be81e332ff5964
Marc Brinkmann <git@marcbrinkmann.de>
2013-12-28 02:58:39 +0100

    Added skeleton of init.sh.

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# this script is run by the bootstrap.sh found on gh-pages. it runs as root
+# using sudo
+
+# to install on a fresh raspbmc:
+#
+# 1. install and start raspbmc
+# 2. select "Exit" from the shutdown menu
+# 3. when prompted, press ESC for a terminal
+# 4. login as "pw", password "raspberry"
+# 5. type in the following command
+#
+#    wget -O- http://mbr.github.io/xvisi/bootstrap.sh  | sh
+#
+# 6. enjoy
+
+REPO_URL=https://github.com/mbr/xvisi
+XVISI_DEPS="python-requests python-lxml"
+
+# update apt repositories
+apt-get update -y
+
+# install git and dependencies
+apt-get install -y git $XVISI_DEPS
