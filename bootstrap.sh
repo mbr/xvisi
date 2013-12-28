ca52a401b1e24042bdd7f4a23ec8e8529efe54c0
Marc Brinkmann <git@marcbrinkmann.de>
2013-12-28 02:47:26 +0100

    Added bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+REMOTE_URL="https://raw.github.com/mbr/xvisi/master/init.sh"
+
+echo "Downloading and running setup script."
+wget -O- $REMOTE_URL | sudo sh
+echo "All good. ;)"
