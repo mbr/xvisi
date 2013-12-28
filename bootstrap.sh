9ecca07909a5f7fc305584b9c7db2ae4b9f5d3bc
Marc Brinkmann <git@marcbrinkmann.de>
2013-12-28 03:17:40 +0100

    Use dialog for output.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ set -e
 REMOTE_URL="https://raw.github.com/mbr/xvisi/master/init.sh"
 
 echo "Downloading and running setup script."
-wget -q -O- $REMOTE_URL | sudo sh
+wget -q -O- $REMOTE_URL | sudo sh 2>&1 | dialog --progressbox 25 80
 echo "All good. ;)"
